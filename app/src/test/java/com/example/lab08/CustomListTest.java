98666e05e02993a57274acda693a5d614a26b116
Minh Bui <nhatbuiminh.1994@gmail.com>
2025-10-28 15:54:53 -0600

    Added Failed Test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,28 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class CustomListTest {
 
+import java.util.ArrayList;
+
+public class CustomListTest {
+    private ArrayList<City> mockCityList() {
+        ArrayList<City> cityList = new ArrayList<City>();
+        cityList.add(mockCity());
+        return cityList;
+    }
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+
+
+    void testHasCity(){
+        ArrayList<City> cityList = mockCityList();
+
+        City city = new City("Langley", "British Columbia");
+        cityList.add(city);
+
+        assertTrue(cityList.hasCity(city));
+        assertFalse(cityList.hasCity(new City("Calgary", "Alberta")));
+
+    }
 }
