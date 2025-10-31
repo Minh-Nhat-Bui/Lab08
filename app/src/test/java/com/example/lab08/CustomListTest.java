3e876bc590a560492163bdf1b1d1ae60374f885f
Minh Bui <nhatbuiminh.1994@gmail.com>
2025-10-31 00:35:59 -0600

    Added Delete function

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,34 +3,31 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-
 public class CustomListTest {
+
     private CustomList mockCityList() {
-        CustomList cityList = new CustomList();
-        cityList.addCity(mockCity());
-        return cityList;
+        return new CustomList();
     }
+
     private City mockCity() {
         return new City("Edmonton", "Alberta");
     }
 
-
-    void testHasCity(){
+    @Test
+    void testHasCity() {
         CustomList cityList = mockCityList();
-
         City city = new City("Langley", "British Columbia");
         cityList.addCity(city);
 
         assertTrue(cityList.hasCity(city));
         assertFalse(cityList.hasCity(new City("Calgary", "Alberta")));
-
     }
 
-    void testDelete(){
+    @Test
+    void testDelete() {
         CustomList cityList = mockCityList();
-
-        City city = new City("Edmonton", "Alberta");
+        City city = new City("Langley", "BC");
+        cityList.addCity(city);
 
         cityList.delete(city);
 
